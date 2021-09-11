e6fa447cb20082813b8c20f26f62783fea35a0ba
Alexander Lee <alxnder.lee@gmail.com>
2021-09-11 15:23:40 +0600

    Create store and presistor

---
+++ src/state/store.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const rootReducer = combineReducers({
+
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+export default store;
